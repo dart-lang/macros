127d47881ae582e3808d8b04cd42829465ad580a
David Morgan <davidmorgan@google.com>
2024-06-21 17:15:52 +0200

    Add generation of comments; add coments to schema; add file for extension methods. (#11)

--- pkgs/dart_model/lib/dart_model.dart
+++ pkgs/dart_model/lib/dart_model.dart
@@ -2,6 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/dart_model.g.dart';
+export 'src/dart_model.dart';
 export 'src/json.dart';
 export 'src/json_changes.dart';
