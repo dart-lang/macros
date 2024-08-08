ac5687c8eac3634b0aa7ac0f322d21b5d8a2bc32
David Morgan <davidmorgan@google.com>
2024-08-08 14:45:59 +0200

    Use package:_macro_host in _cfe_macros.

--- pkgs/_cfe_macros/lib/macro_implementation.dart
+++ pkgs/_cfe_macros/lib/macro_implementation.dart
@@ -3,21 +3,21 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:_macro_host/macro_host.dart';
-// ignore: implementation_imports
-import 'package:analyzer/src/summary2/macro_injected_impl.dart' as injected;
 import 'package:dart_model/dart_model.dart';
+// ignore: implementation_imports
+import 'package:front_end/src/macros/macro_injected_impl.dart' as injected;
 import 'package:macro_service/macro_service.dart';
 import 'package:macros/macros.dart';
 // ignore: implementation_imports
 import 'package:macros/src/executor.dart' as injected;
 
 /// Injected macro implementation for the analyzer.
-class AnalyzerMacroImplementation implements injected.MacroImplementation {
+class CfeMacroImplementation implements injected.MacroImplementation {
   final Uri packageConfig;
   final Map<String, String> macroImplByName;
   final MacroHost _host;
 
-  AnalyzerMacroImplementation._(
+  CfeMacroImplementation._(
       this.packageConfig, this.macroImplByName, this._host);
 
   /// Starts the macro host.
@@ -28,68 +28,68 @@ class AnalyzerMacroImplementation implements injected.MacroImplementation {
   /// [macroImplByName] identifies macros, it's a placeholder until we identify
   /// macros using package config. Keys are macro annotation qualified names
   /// (`uri#name`) and values are macro implementation qualified names.
-  static Future<AnalyzerMacroImplementation> start({
+  static Future<CfeMacroImplementation> start({
     required Uri packageConfig,
     required Map<String, String> macroImplByName,
   }) async =>
-      AnalyzerMacroImplementation._(
+      CfeMacroImplementation._(
           packageConfig,
           macroImplByName,
           await MacroHost.serve(
               macroImplByName: macroImplByName,
-              queryService: AnalyzerQueryService()));
+              queryService: CfeQueryService()));
 
   @override
   injected.MacroPackageConfigs get packageConfigs =>
-      AnalyzerMacroPackageConfigs(this);
+      CfeMacroPackageConfigs(this);
 
   @override
-  injected.MacroRunner get runner => AnalyzerMacroRunner(this);
+  injected.MacroRunner get macroRunner => CfeMacroRunner(this);
 }
 
-class AnalyzerQueryService implements QueryService {
+class CfeQueryService implements QueryService {
   @override
   Future<QueryResponse> handle(QueryRequest request) async {
     return QueryResponse();
   }
 }
 
-class AnalyzerMacroPackageConfigs implements injected.MacroPackageConfigs {
-  final AnalyzerMacroImplementation _impl;
+class CfeMacroPackageConfigs implements injected.MacroPackageConfigs {
+  final CfeMacroImplementation _impl;
 
-  AnalyzerMacroPackageConfigs(this._impl);
+  CfeMacroPackageConfigs(this._impl);
 
   @override
   bool isMacro(Uri uri, String name) =>
       _impl._host.isMacro(_impl.packageConfig, QualifiedName('$uri#$name'));
 }
 
-class AnalyzerMacroRunner implements injected.MacroRunner {
-  final AnalyzerMacroImplementation _impl;
+class CfeMacroRunner implements injected.MacroRunner {
+  final CfeMacroImplementation _impl;
 
-  AnalyzerMacroRunner(this._impl);
+  CfeMacroRunner(this._impl);
 
   @override
-  injected.RunningMacro run(Uri uri, String name) => AnalyzerRunningMacro.run(
+  injected.RunningMacro run(Uri uri, String name) => CfeRunningMacro.run(
       _impl,
       QualifiedName('$uri#$name'),
       // Look up from the macro name to its implementation.
       QualifiedName(_impl.macroImplByName['$uri#$name']!));
 }
 
-class AnalyzerRunningMacro implements injected.RunningMacro {
-  final AnalyzerMacroImplementation _impl;
+class CfeRunningMacro implements injected.RunningMacro {
+  final CfeMacroImplementation _impl;
   final QualifiedName name;
   final QualifiedName implementation;
   late final Future _started;
 
-  AnalyzerRunningMacro._(this._impl, this.name, this.implementation);
+  CfeRunningMacro._(this._impl, this.name, this.implementation);
 
   // TODO(davidmorgan): should this be async, removing the need for `_started`?
   // If so the API injected into analyzer+CFE needs to change to be async.
-  static AnalyzerRunningMacro run(AnalyzerMacroImplementation impl,
-      QualifiedName name, QualifiedName implementation) {
-    final result = AnalyzerRunningMacro._(impl, name, implementation);
+  static CfeRunningMacro run(CfeMacroImplementation impl, QualifiedName name,
+      QualifiedName implementation) {
+    final result = CfeRunningMacro._(impl, name, implementation);
     // TODO(davidmorgan): this is currently what starts the macro running,
     // make it explicit.
     result._started =
@@ -98,28 +98,26 @@ class AnalyzerRunningMacro implements injected.RunningMacro {
   }
 
   @override
-  Future<AnalyzerMacroExecutionResult> executeDeclarationsPhase(
-      MacroTarget target,
+  Future<CfeMacroExecutionResult> executeDeclarationsPhase(MacroTarget target,
       DeclarationPhaseIntrospector declarationsPhaseIntrospector) async {
     await _started;
-    return AnalyzerMacroExecutionResult(
+    return CfeMacroExecutionResult(
         target, await _impl._host.augment(name, AugmentRequest(phase: 2)));
   }
 
   @override
-  Future<AnalyzerMacroExecutionResult> executeDefinitionsPhase(
-      MacroTarget target,
+  Future<CfeMacroExecutionResult> executeDefinitionsPhase(MacroTarget target,
       DefinitionPhaseIntrospector definitionPhaseIntrospector) async {
     await _started;
-    return AnalyzerMacroExecutionResult(
+    return CfeMacroExecutionResult(
         target, await _impl._host.augment(name, AugmentRequest(phase: 3)));
   }
 
   @override
-  Future<AnalyzerMacroExecutionResult> executeTypesPhase(
+  Future<CfeMacroExecutionResult> executeTypesPhase(
       MacroTarget target, TypePhaseIntrospector typePhaseIntrospector) async {
     await _started;
-    return AnalyzerMacroExecutionResult(
+    return CfeMacroExecutionResult(
         target, await _impl._host.augment(name, AugmentRequest(phase: 1)));
   }
 }
@@ -128,11 +126,11 @@ class AnalyzerRunningMacro implements injected.RunningMacro {
 ///
 /// TODO(davidmorgan): add to `AugmentationResponse` to cover all the
 /// functionality of `MacroExecutionResult`.
-class AnalyzerMacroExecutionResult implements injected.MacroExecutionResult {
+class CfeMacroExecutionResult implements injected.MacroExecutionResult {
   final MacroTarget target;
   final AugmentResponse augmentResponse;
 
-  AnalyzerMacroExecutionResult(this.target, this.augmentResponse);
+  CfeMacroExecutionResult(this.target, this.augmentResponse);
 
   @override
   List<Diagnostic> get diagnostics => [];
@@ -167,8 +165,11 @@ class AnalyzerMacroExecutionResult implements injected.MacroExecutionResult {
         // TODO(davidmorgan): this assumes augmentations are for the macro
         // application target. Instead, it should be explicit in
         // `AugmentResponse`.
-        (target as Declaration).identifier: augmentResponse.augmentations
-            .map((a) => DeclarationCode.fromParts([a.code]))
-            .toList(),
+        // TODO(davidmorgan): empty augmentations response breaks the test,
+        // it's not clear why.
+        if (augmentResponse.augmentations.isNotEmpty)
+          (target as Declaration).identifier: augmentResponse.augmentations
+              .map((a) => DeclarationCode.fromParts([a.code]))
+              .toList(),
       };
 }
