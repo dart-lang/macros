545f1a51c5fa5946fe9f731ded5d00c4b6491905
David Morgan <davidmorgan@google.com>
2024-10-25 16:16:29 +0200

    Remove json.dart and json_changes.dart.

--- pkgs/dart_model/lib/dart_model.dart
+++ pkgs/dart_model/lib/dart_model.dart
@@ -3,8 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 export 'src/dart_model.dart';
-export 'src/json.dart';
-export 'src/json_changes.dart';
 export 'src/lazy_merged_map.dart' show MergeModels;
 export 'src/scopes.dart';
 export 'src/type.dart';
